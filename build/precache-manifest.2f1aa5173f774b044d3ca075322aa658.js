b643cc77fd5747d5f0a9bc0ad5ac5f983eaa622e
王一兰 <44659737+Wang-Yilan@users.noreply.github.com>
2020-10-18 20:21:38 +0800

    Add files via upload

---
+++ build/precache-manifest.2f1aa5173f774b044d3ca075322aa658.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b830199d52a0111d5d99fb8a68a25c6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "69ce0df5274dca338288",
+    "url": "/static/css/2.f98a1de1.chunk.css"
+  },
+  {
+    "revision": "5017eb12aec5c4be878a",
+    "url": "/static/css/main.08453741.chunk.css"
+  },
+  {
+    "revision": "69ce0df5274dca338288",
+    "url": "/static/js/2.7f7def15.chunk.js"
+  },
+  {
+    "revision": "4d47ffde3dc69784e8e23fd910817ef8",
+    "url": "/static/js/2.7f7def15.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5017eb12aec5c4be878a",
+    "url": "/static/js/main.76ebf7e2.chunk.js"
+  },
+  {
+    "revision": "e2702865a21868f2ebe6",
+    "url": "/static/js/runtime-main.7918a0be.js"
+  }
+]);
